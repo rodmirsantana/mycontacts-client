961f28745fb40479d97de0e5cde03dfb773aa40c
rodmirsantana <rodmirandasantana@gmail.com>
2024-01-31 12:06:54 -0300

    treat API errors; create custom APIError

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -1,4 +1,5 @@
 import delay from "../../utils/delay";
+import APIError from "../../errors/APIError";
 
 class HttpClient {
   constructor(baseURL) {
@@ -6,11 +7,21 @@ class HttpClient {
   }
 
   async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
-
     await delay(500);
 
-    return response.json();
+    const response = await fetch(`${this.baseURL}${path}`);
+
+    let body = null;
+    const contentType = response.headers.get("Content-Type");
+    if (contentType.includes("application/json")) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+
+    throw new APIError(response, body);
   }
 }
 
