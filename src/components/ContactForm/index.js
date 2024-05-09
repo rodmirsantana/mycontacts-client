5dfb3ad833e9ddc097e3f804febd9b151e202f66
rodmirsantana <rodmirandasantana@gmail.com>
2024-05-09 18:14:08 -0300

    implement create Contact; refactor http client

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -13,7 +13,7 @@ import Input from "../Input";
 import Select from "../Select";
 import Button from "../Button";
 
-export default function ContactForm({ buttonLabel }) {
+export default function ContactForm({ buttonLabel, onSubmit }) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -69,6 +69,8 @@ export default function ContactForm({ buttonLabel }) {
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    onSubmit({ name, email, phone, categoryId });
   }
 
   return (
@@ -76,7 +78,6 @@ export default function ContactForm({ buttonLabel }) {
       <FormGroup error={getErrorMessageByFieldName("name")}>
         <Input
           error={getErrorMessageByFieldName("name")}
-          // Controlled component
           placeholder="Nome *"
           value={name}
           onChange={handleNameChange}
@@ -129,4 +130,5 @@ export default function ContactForm({ buttonLabel }) {
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
