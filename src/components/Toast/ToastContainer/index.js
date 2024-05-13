9df12b4a5a2a11dc913f39cda353a30b2d1980e6
rodmirsantana <rodmirandasantana@gmail.com>
2024-05-13 15:41:14 -0300

    create Toast container and message components; add styles

---
+++ src/components/Toast/ToastContainer/index.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Container } from "./styles";
+
+import ToastMessage from "../ToastMessage";
+
+export default function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: "default", text: "Default text" },
+    { id: Math.random(), type: "success", text: "Success text" },
+    { id: Math.random(), type: "danger", text: "Danger text" },
+  ]);
+
+  return (
+    <Container>
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
+    </Container>
+  );
+}
