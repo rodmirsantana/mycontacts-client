87e98e6b927dd2129fa9c10351580edc03ee6df7
rodmirsantana <rodmirandasantana@gmail.com>
2024-05-19 11:27:58 -0300

    create useAnimatedUnmount hook and use it in Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,23 +3,18 @@ import PropTypes from "prop-types";
 import { Overlay } from "./styles";
 import Spinner from "../Spinner";
 import ReactPortal from "../ReactPortal";
+import useAnimatedUnmount from "../../hooks/useAnimatedUnmount";
 
 export default function Loader({ isLoading }) {
-  if (!isLoading) {
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(isLoading);
+
+  if (!shouldRender) {
     return null;
-  }
-
-  let container = document.getElementById("loader-root");
-
-  if (!container) {
-    container = document.createElement("div");
-    container.setAttribute("id", "loader-root");
-    document.body.appendChild(container);
   }
 
   return (
     <ReactPortal containerId="loader-root">
-      <Overlay>
+      <Overlay isLeaving={!isLoading} ref={animatedElementRef}>
         <Spinner size={90} />
       </Overlay>
     </ReactPortal>
