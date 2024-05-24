826620ac1dfc8ea5d31bfd040bab109c1126eea5
rodmirsantana <rodmirandasantana@gmail.com>
2024-05-24 12:00:23 -0300

    remove defaultProps and use default JS parameters instead

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,12 +3,12 @@ import { StyledButton } from "./styles";
 import Spinner from "../Spinner";
 
 export default function Button({
-  type,
-  disabled,
-  isLoading,
+  type = "button",
+  disabled = false,
+  isLoading = false,
   children,
-  danger,
-  onClick,
+  danger = false,
+  onClick = undefined,
 }) {
   return (
     <StyledButton
@@ -30,12 +30,4 @@ Button.propTypes = {
   children: PropTypes.node.isRequired,
   danger: PropTypes.bool,
   onClick: PropTypes.func,
-};
-
-Button.defaultProps = {
-  type: "button",
-  disabled: false,
-  isLoading: false,
-  danger: false,
-  onClick: undefined,
 };
